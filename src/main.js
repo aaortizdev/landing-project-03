8fcbfaf56dbcb7f4ddfb655688fa07c6d086d6bb
Ariel Ortiz <aaortiz@espol.edu.ec>
2026-06-20 16:41:10 +0000

    Agregar sección de contacto en Agendar

--- src/main.js
+++ src/main.js
@@ -1,31 +1,32 @@
-
 document.addEventListener('DOMContentLoaded', () => {
     console.log("Sistema DENTAL inicializado.");
 
     const formCita = document.getElementById('formulario-cita');
     const seccionResultado = document.getElementById('resultado-interaccion');
     
+    // Elementos donde imprimiremos la información
     const resNombre = document.getElementById('res-nombre');
+    const resTelefono = document.getElementById('res-telefono'); // Nuevo
     const resTratamiento = document.getElementById('res-tratamiento');
 
     if (formCita) {
         formCita.addEventListener('submit', (e) => {
             e.preventDefault();
 
+            // Obtenemos los valores de los inputs
             const nombreIngresado = document.getElementById('nombrePaciente').value;
+            const telefonoIngresado = document.getElementById('telefonoPaciente').value; // Nuevo
             const tratamientoSeleccionado = document.getElementById('tratamientoInteres').value;
 
-            // Interactuamos con el DOM para mostrar los datos
+            // Interactuamos con el DOM
             resNombre.textContent = nombreIngresado;
+            resTelefono.textContent = telefonoIngresado; // Nuevo
             resTratamiento.textContent = tratamientoSeleccionado;
 
-            // Hacemos visible la sección de resultados
+            // Hacemos visible la sección
             seccionResultado.classList.remove('hidden');
-
-            // Hacemos scroll suave hasta la nueva sección para mejor UX
             seccionResultado.scrollIntoView({ behavior: 'smooth' });
             
-            // Opcional: resetear el formulario
             formCita.reset();
         });
     }
